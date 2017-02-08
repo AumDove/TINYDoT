9f048f7ea9ac51b3901ce4ca9ce30704dfc5ebaa
aumdove <aumdove13@gmail.com>
2017-02-07 14:38:57 -1000

    add twentyseventeen theme

--- wp-content/themes/meg/header.php
+++ wp-content/themes/meg/header.php
@@ -52,10 +52,10 @@
 			if ( is_front_page()) : ?>
                         <div class="site-branding">
                             <div class="full-header"><!-- .full-header -->
-                                 <p id="front-decoration">Hello, world!</p>
+                                 <p id="front-decoration"></p>
                                 <img id="headshot-image" src="<?php echo get_bloginfo('template_url') ?>/img/headshot-meg-two.jpg"/> 
-                                <h1 id="hero-text">I&apos;m Meg ...</h1>
-                                <p id="full-hero-text">I build functional, simple websites for people who are ready to get started on the web. Using developer skills, I help marketers and small business owners find their place on the web. Quickly.</p> 
+                                <h1 id="hero-text">Welcome to TINYDoT</h1>
+                                <p id="full-hero-text">Here at TINYDoT, we understand how difficult it can be to keep up with the giants of the world. With this in mind, we build functional, simple websites for people who are ready to stake their claim. Simply. Quickly. Let's get your tiny dot on the map!</p> 
 
                                 <div class="flex-container-nav">    
                                     <a id="button-one" href="#skip-to-featured" class="nav-button" >Work</a>
@@ -66,7 +66,7 @@
                         
                      <?php else : ?>
                            <div class="reduced-header">
-                             <h1 id="hero-text">I&apos;m Meg ...</h1>
+                             <h1 id="hero-text">Welcome to TINYDoT</h1>
                     <?php endif; ?><!--end reduced header section-->
 		
                         
